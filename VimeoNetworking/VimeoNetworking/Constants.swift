4bc46b091aa0c3f8dc57b7d3fb0c2137ffd960cc
Mike Westendorf <mikew@vimeo.com>
2016-11-30 15:50:06 -0500

    pointing to the test server.

--- VimeoNetworking/VimeoNetworking/Constants.swift
+++ VimeoNetworking/VimeoNetworking/Constants.swift
@@ -27,7 +27,7 @@
 import Foundation
 
  /// Base URL for the Vimeo API
-public let VimeoBaseURLString = NSURL(string: "https://api.vimeo.com")
+public let VimeoBaseURLString = NSURL(string: "https://cinema-api.ci.vimeows.com")
 
  /// Default API version to use for requests
 public let VimeoDefaultAPIVersionString = "3.2"
